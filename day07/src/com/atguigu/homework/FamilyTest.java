da47d2e461cff908194a97d937f2cd445d855472
oono <andychao3210@gmail.com>
2020-09-01 19:33:32 +0800

    2020.9.1 commits

--- day07/src/com/atguigu/homework/FamilyTest.java
+++ day07/src/com/atguigu/homework/FamilyTest.java
@@ -9,15 +9,15 @@ public class FamilyTest {
     public static void main(String[] args) {
 
         Husband hus = new Husband();
+        Wife wife = new Wife();
+
         hus.name = "xiaoming";
         hus.age = 19;
-
-        Wife wife = new Wife();
-        wife.name = "xiaohong";
+        hus.wife = wife;
+        wife.name = "xia1ng";
         wife.age = 20;
         wife.husband = hus;
 
-        hus.wife = wife;
 
         hus.getInfo();
         wife.getInfo();
